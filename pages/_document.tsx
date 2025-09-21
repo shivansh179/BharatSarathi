d13061aafd2b06f21a07a6e6a4152b9787ac4f68
shivansh179 <prasantshukla89@gmail.com>
2025-09-21 13:56:54 +0530

    changedn favicon and updated description also app thumbnail

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
-        {/* iOS meta tags */}
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-        <meta name="apple-mobile-web-app-title" content="My App" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+  <link rel="manifest" href="/manifest.json" />
+  <meta name="theme-color" content="#000000" />
+  <link rel="icon" href="/bharat sarathi logo.png" type="image/png" />
+  {/* iOS meta tags */}
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+  <meta name="apple-mobile-web-app-title" content="Bharat Sarathi" />
+  <link rel="apple-touch-icon" href="/bharat sarathi logo.png" />
       </Head>
       <body className="antialiased">
         <Main />
